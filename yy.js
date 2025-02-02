3098337922245e7f170913b45e247a3b02a83d60
ymshi <ymshi0420@gmail.com>
2025-02-02 20:58:24 +0800

    Create yy.js

---
+++ yy.js
@@ -0,0 +1,27 @@
+/*************************************
+
+项目名称：创
+更新日期：2025-01-08
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/y.weixinmy.com\/appstore) url script-response-body https://raw.githubusercontent.com/ymmshi/qx/main/yy.js
+[mitm]
+hostname = y.weixinmy.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+const main = /appstore/;
+
+
+if(main.test($request.url)){
+  chxm1023 = {
+    "msg" : "恢复成功",
+    "code" : 1
+  };
+}
+
+$done({body : JSON.stringify(chxm1023)});
